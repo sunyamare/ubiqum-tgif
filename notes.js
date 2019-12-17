f7b6d9d8d3adad0502e41eaad8e38116b657c2c6
sunyamare <sunyamare@gmail.com>
2019-12-17 09:41:54 +0100

    step 1 TGIF

--- notes.js
+++ notes.js
@@ -1,5 +1,5 @@
 // Construct a URL for the ProPublica Congress web service to get the members of the Senate for the 113th Congress.
 /*
 curl "https://api.propublica.org/congress/v1/113/senate/members.json"
-    - H "X-API-Key: yM7WFa1TO8Y2eJQxD31fGVolfpMVNmqu9XOjBvfq";
+    - H "X-API-Key: ...";
 */
