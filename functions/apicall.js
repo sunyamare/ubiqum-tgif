4d9d17160b55c8452a6c60cffca6af4848f31a0f
sunyamare <sunyamare@gmail.com>
2020-01-13 14:49:06 +0100

    removed comments

--- functions/apicall.js
+++ functions/apicall.js
@@ -17,22 +17,3 @@ exports.handler = async event => {
         console.log(error);
     }
 };
-
-/*const fetch = require("node-fetch").default;
-
-exports.handler = async event => {
-    const chamber = event.queryStringParameters.c;
-    const url = `https://api.propublica.org/congress/v1/113/${chamber}/members.json`;
-
-    const response = await fetch(url, {
-        headers: {
-            "X-API-Key": process.env.API_KEY
-        }
-    })
-        .then(response => response.json())
-        .then(response => JSON.stringify(response));
-    return {
-        statusCode: 200,
-        body: response
-    };
-};*/
