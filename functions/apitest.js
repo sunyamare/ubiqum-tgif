fc7984f0f6ab1f30c297bde593492238568d3594
sunyamare <sunyamare@gmail.com>
2020-01-13 11:53:16 +0100

    added api lambda function with query string param

--- functions/apitest.js
+++ functions/apitest.js
@@ -1,8 +1,19 @@
-// .netlify/functions/apitest.js
-exports.handler = async () => {
-    const envvar = process.env.API_KEY;
+// .netlify/functions/apitest
+import fetch from "node-fetch";
+
+exports.handler = async event => {
+    const chamber = event.queryStringParameters.c;
+    const url = `https://api.propublica.org/congress/v1/113/${chamber}/members.json`;
+
+    const response = await fetch(url, {
+        headers: {
+            "X-API-Key": process.env.API_KEY
+        }
+    })
+        .then(response => response.json())
+        .then(response => JSON.stringify(response));
     return {
         statusCode: 200,
-        body: `API Key: ${envvar}!`
+        body: response
     };
 };
