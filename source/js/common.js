9f1baa570f2d8b875a5106710db41c2c2cf02a69
sunyamare <sunyamare@gmail.com>
2020-01-14 14:25:31 +0100

    combined back to top and api call js files

--- source/js/common.js
+++ source/js/common.js
@@ -17,3 +17,19 @@ const removeLoader = () => {
         element.classList.remove("loader");
     }
 };
+
+// Show back to top button only when scrolling
+const mybutton = document.getElementById("top-button");
+window.onscroll = function() {
+    scrollFunction();
+};
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 40 ||
+        document.documentElement.scrollTop > 40
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
